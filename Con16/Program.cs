ba32c3fd86c6172bcb497a3336a01a767f50d019
Marek Lewiński <lewinski.marek@outlook.com>
2016-06-08 11:58:41 +0200

    Conditional #16

---
+++ Con16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Con16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = Convert.ToChar(Console.ReadLine());
+            string type = "";
+            if (a < 65 || a > 122 || (a > 90 && a < 97)) type = "illegal symbol";
+            else
+            {
+                a = Char.ToLower(a);
+                char[] vowels = {'a', 'e', 'u', 'i', 'o', 'p'};
+                if (vowels.Contains(a)) type = "vowel";
+                else type = "consonant";
+            }
+            Console.WriteLine("The alphabet is {0}", type);
+            Console.ReadLine();
+        }
+    }
+}
