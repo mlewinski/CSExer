f8664d3c3ee8a7b5dcab4627414cb0a04afd72d5
Marek Lewiński <lewinski.marek@outlook.com>
2016-06-08 12:45:56 +0200

    Conditional #19

---
+++ Con19/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Con19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char markSymbol = Convert.ToChar(Console.ReadLine());
+            string mark = "";
+            switch (markSymbol)
+            {
+                case 'V':
+                    mark = "Very good";
+                    break;
+                case 'E':
+                    mark = "Excellent";
+                    break;
+                case 'G':
+                    mark = "Good";
+                    break;
+                case 'A':
+                    mark = "Average";
+                    break;
+                case 'F':
+                    mark = "Fail";
+                    break;
+                default:
+                    mark = "Unknown";
+                    break;
+            }
+            Console.WriteLine("Your mark is {0} which means {1}", markSymbol, mark);
+            Console.ReadLine();
+        }
+    }
+}
