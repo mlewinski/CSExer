8d00e649895e4b1409df2459f67c59a451c823a5
Marek Lewiński <lewinski.marek@outlook.com>
2016-06-08 12:49:18 +0200

    Conditional #20

---
+++ Con20/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Con20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+            int dayNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("It's {0}", days[Math.Abs(dayNumber-1)%7]);
+            Console.ReadLine();
+        }
+    }
+}
