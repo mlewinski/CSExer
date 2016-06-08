daee73ea49858632fe47584980697b184bd5068c
Marek Lewiński <lewinski.marek@outlook.com>
2016-06-08 11:36:25 +0200

    Conditional #15

---
+++ Con15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Con15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Triangle is{0} valid",(Math.Abs((a+b+c) - 180) > Double.Epsilon)?" not":"");
+            Console.ReadLine();
+        }
+    }
+}
