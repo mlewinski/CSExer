0a4c85210d41f3fb50073eaf91d51484b28b934d
Marek Lewiński <lewinski.marek@outlook.com>
2016-06-08 11:29:27 +0200

    Conditional #14

---
+++ Con14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Con14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            string typeOfTriangle = "";
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            if (Compare(a,b))
+            {
+                if (Compare(b,c)) typeOfTriangle = "equilateral";
+                else typeOfTriangle = "isosceles";
+            }
+            else if (Compare(b, c)) typeOfTriangle = "isosceles";
+            else typeOfTriangle = "scalene";
+            Console.WriteLine("Triangle is {0}", typeOfTriangle);
+            Console.ReadLine();
+        }
+
+        static bool Compare(double a, double b)
+        {
+            if (Math.Abs(a - b) < 0.0000001) return true;
+            return false;
+        }
+    }
+}
